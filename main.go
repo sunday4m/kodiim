b5beb617c31b31ce15a7faa61363a0b4798dd5e9
davydov.v29 <davydov.v29@rwb.ru>
2025-05-02 14:39:40 +0300

    change 200 to 300

--- main.go
+++ main.go
@@ -16,6 +16,6 @@ func main() {
 	for i := 1; i <= 5; i++ {
 		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
 		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 200/i)
+		fmt.Println("i =", 300/i)
 	}
 }
